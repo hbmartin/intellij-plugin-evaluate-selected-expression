b7f963b0c906daf6ccb2109d0f6c0cde432f7a6b
GitHub Action <action@github.com>
2022-11-13 16:27:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/hbmartin/intellijpluginevaluateselectedexpression/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hbmartin/intellijpluginevaluateselectedexpression/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hbmartin.intellijpluginevaluateselectedexpression.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hbmartin.intellijpluginevaluateselectedexpression.MyBundle
 
 class MyProjectService(project: Project) {
 
